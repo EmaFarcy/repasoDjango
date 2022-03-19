1139dcfac7d80aa63f51121b244386e360df9b06
Emanuel Farcy <emanuelfarcy@gmail.com>
2022-03-18 23:58:48 -0300

    ultima clase after preentrega

--- profesionales/models.py
+++ profesionales/models.py
@@ -6,6 +6,10 @@ class Cerrajero(models.Model):
     nombre= models.CharField(max_length=20)
     apellido= models.CharField(max_length=30)
     desempleado= models.BooleanField()
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+    
 
 class Futbolista(models.Model):
     nombre= models.CharField(max_length=20)
---
+++ profesionales/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cerrajero/crear/', views.crear_cerrajero, name="crear_cerrajero"),
+    path('cerrajeros/', views.lista_cerrajeros, name="lista_cerrajeros"),
+    # path('', name="buscar_cerrajero")
+]
